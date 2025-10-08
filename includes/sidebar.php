a62ebad5d5e4cd38f6215c590af3cf9629bd44b3
ddroy9090-ind <dev@reliantsurveyors.com>
2025-10-08 17:47:18 +0530

    Revamp sidebar UI styling

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,55 +1,60 @@
 <aside class="sidebar" id="sidebar">
-    <div class="sidebar-header">
-        <a href="index.php" class="sidebar-logo" aria-label="TeleCRM home">
-            <img src="assets/images/logo/logo.svg" alt="TeleCRM" />
-        </a>
+    <div class="sidebar-brand">
+        <div class="sidebar-brand-icon">
+            <i class='bx bx-buildings'></i>
+        </div>
+        <div class="sidebar-brand-details">
+            <span class="sidebar-brand-title">TeleCRM</span>
+            <span class="sidebar-brand-subtitle">CRM Suite</span>
+        </div>
     </div>
-    <ul class="sidebar-menu">
-        <li>
-            <a href="index.php" class="sidebar-link">
-                <i class="bx bx-home-alt"></i>
-                <span class="sidebar-text">Dashboard</span>
-            </a>
-        </li>
-        <li>
-            <a href="users.php" class="sidebar-link">
-                <i class="bx bx-user"></i>
-                <span class="sidebar-text">Users</span>
-            </a>
-        </li>
-        <li class="sidebar-dropdown">
-            <button type="button" class="sidebar-link sidebar-dropdown-toggle">
-                <i class="bx bx-group"></i>
-                <span class="sidebar-text">Leads</span>
-                <i class="bx bx-chevron-down sidebar-dropdown-arrow"></i>
-            </button>
-            <ul class="sidebar-submenu">
-                <li>
-                    <a href="all-leads.php" class="sidebar-link">
-                        <i class="bx bx-list-ul"></i>
-                        <span class="sidebar-text">All Leads</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="my-leads.php" class="sidebar-link">
-                        <i class="bx bx-user-circle"></i>
-                        <span class="sidebar-text">My Leads</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="add-leads.php" class="sidebar-link">
-                        <i class="bx bx-user-plus"></i>
-                        <span class="sidebar-text">Add Leads</span>
-                    </a>
-                </li>
-            </ul>
-        </li>
-        <li class="mt-auto">
-            <a href="logout.php" class="sidebar-link" onclick="return confirm('Are you sure you want to logout?')">
-                <i class="bx bx-log-out"></i>
-                <span class="sidebar-text">Logout</span>
-            </a>
-        </li>
-    </ul>
+    <nav class="sidebar-nav">
+        <ul class="sidebar-menu">
+            <li>
+                <a href="index.php" class="sidebar-link">
+                    <span class="sidebar-icon"><i class="bx bx-home-alt"></i></span>
+                    <span class="sidebar-text">Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="users.php" class="sidebar-link">
+                    <span class="sidebar-icon"><i class="bx bx-user"></i></span>
+                    <span class="sidebar-text">Users</span>
+                </a>
+            </li>
+            <li class="sidebar-dropdown">
+                <button type="button" class="sidebar-link sidebar-dropdown-toggle">
+                    <span class="sidebar-icon"><i class="bx bx-group"></i></span>
+                    <span class="sidebar-text">Leads</span>
+                    <i class="bx bx-chevron-down sidebar-dropdown-arrow"></i>
+                </button>
+                <ul class="sidebar-submenu">
+                    <li>
+                        <a href="all-leads.php" class="sidebar-link">
+                            <span class="sidebar-icon"><i class="bx bx-list-ul"></i></span>
+                            <span class="sidebar-text">All Leads</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="my-leads.php" class="sidebar-link">
+                            <span class="sidebar-icon"><i class="bx bx-user-circle"></i></span>
+                            <span class="sidebar-text">My Leads</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="add-leads.php" class="sidebar-link">
+                            <span class="sidebar-icon"><i class="bx bx-user-plus"></i></span>
+                            <span class="sidebar-text">Add Leads</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="sidebar-logout mt-auto">
+                <a href="logout.php" class="sidebar-link" onclick="return confirm('Are you sure you want to logout?')">
+                    <span class="sidebar-icon"><i class="bx bx-log-out"></i></span>
+                    <span class="sidebar-text">Logout</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
 </aside>
-
